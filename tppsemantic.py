cd8a54f19009447acdf05b8ea70027a45a0f5627
CaioCintra <caiocintra2000@gmail.com>
2022-05-15 21:02:34 -0300

    Período Jurássico

--- tppsemantic.py
+++ tppsemantic.py
@@ -1,11 +1,14 @@
 import tppparser
 import pandas as pd
 
-global dataFrame 
-dataFrame = pd.DataFrame(data = [], columns = ['TOKEN', 'LEXEMA', 'TIPO'])
+global dataFrameVar 
+dataFrameVar = pd.DataFrame(data = [], columns = ['TOKEN', 'LEXEMA', 'TIPO'])
+global dataFrameFunc 
+dataFrameFunc = pd.DataFrame(data = [], columns = ['TOKEN', 'LEXEMA', 'PARAMETROS', 'TIPO'])
 
 def treeTravel(root):
-    global dataFrame 
+    global dataFrameVar 
+    global dataFrameFunc
     listNode = ''
     for node in root.children:
         #print(node.label)
@@ -13,8 +16,14 @@ def treeTravel(root):
             token = node.children[2].children[0].children[0].label
             lexema = node.children[2].children[0].children[0].children[0].label
             tipo = node.children[0].children[0].label
-            dataFrame = dataFrame.append({'TOKEN' : token, 'LEXEMA' : lexema, 'TIPO' : tipo}, ignore_index=True)
+            dataFrameVar = dataFrameVar.append({'TOKEN' : token, 'LEXEMA' : lexema, 'TIPO' : tipo}, ignore_index=True)
 
+        if (node.label == 'declaracao_funcao'):
+            tipo = node.children[0].children[0].label
+            token = node.children[1].children[0].label
+            lexema = node.children[1].children[0].children[0].label
+            parametros = 0
+            dataFrameFunc = dataFrameFunc.append({'TOKEN' : token, 'LEXEMA' : lexema, 'PARAMETROS' : parametros, 'TIPO' : tipo}, ignore_index=True)
         listNode = treeTravel(node)
     return listNode
 
@@ -22,7 +31,12 @@ def main():
     tree = tppparser.main()
 
     treeCall = treeTravel(tree)
-    print (dataFrame)
+    print ('\nTabela de Símbolos')
+    print (dataFrameVar)
+
+    print ('\nTabela de Funções')
+    print (dataFrameFunc)
+    
 
 if __name__ == "__main__":
     main()
